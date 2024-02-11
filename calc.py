a3bf95529270b7859b18a12e79d7d675c63a2f8d
amoynahan89 <amfinsov@gmail.com>
2024-02-10 22:54:45 -0800

    Better

--- calc.py
+++ calc.py
@@ -1,14 +1,14 @@
-
+# Main function 
 def main():
     gross_bill = get_bill()
     residents = get_residents()
     per_person = split_bill(gross_bill, residents)
-    appt_share = per_appt(per_person)
-    print(appt_share)
-    #add_dues()
-    #subtract_expenses()
+    ea_appt_share = per_appt(per_person)
+    #net_bill = contributions(ea_appt_share)
+    print(ea_appt_share)
 
 
+# Convert inputs to floats
 def verify_input(prompt):
     while True:
         try:
@@ -18,6 +18,7 @@ def verify_input(prompt):
             print("You must enter a number.")
 
 
+# Get sum of all house expenses. Becomes gross_bill
 def get_bill():
     total_bill = []
     while True:
@@ -30,17 +31,20 @@ def get_bill():
                 break       
             elif answer == "no":
                 return sum(total_bill)
-            
 
+
+# Number of residents
 def get_residents():
     return verify_input("How many people were home this month? ")
 
 
+# gross_bill split by number of residents. Becomes per_person
 def split_bill(gross_bill, residents):
     individual_bill = gross_bill / residents
     return individual_bill
     
 
+# Returns a bill for each appartment. Becomes ea_appt_share
 def per_appt(per_person):
     bill = {}
     for res in current_residents:
@@ -48,15 +52,15 @@ def per_appt(per_person):
     return bill
    
 
-#def get_deductions():
+#def contributions(appt_share):
 
-
+# Names and how many in each appt.
 current_residents = [
     {"name": "A + A", "multiplier": 2},
     {"name": "S + J", "multiplier": 1.5},
     {"name": "N + P", "multiplier": 1.5},
-    #{"name": "G", "multiplier": 1},
+    {"name": "G", "multiplier": 1},
 ]
 
-
+# Call main
 main()
